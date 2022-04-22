b9e104755608573a6336ff57754c75bfd27a225a
kriepie krollie <32949124+kriepiekrollie@users.noreply.github.com>
2022-04-22 09:59:48 +0200

    snake

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+cubes = [];
+X = [];
+Y = [];
+
 function createCube(x, y) {
     result = document.createElement("div");
     left = document.createElement("div");
@@ -14,11 +18,49 @@ function createCube(x, y) {
     right.className = "RomRight";
     topp.className = "RomTop";
 
-    result.style = "left:calc(50% + "+x+"px); top:calc(50% + "+y+"px);";
+    result.style = "left:calc(50% + "+((x - y) * 121.24355653)+"px); top:calc(50% + "+(x + y) * 70+"px);";
+
+    cubes.push(result)
+    X.push(x);
+    Y.push(y);
 
     return result;
 }
 
-for (i = -3; i <= 3; i++)
-    for (j = Math.abs(i) - 3; j <= 3 - Math.abs(i); j++)
-        createCube((i - j) * 121.24355653, (i + j) * 70);
+createCube(0, 0);
+dx = 1;
+dy = 0;
+
+document.onkeydown = function(e) {
+    switch (e.key) {
+        case "ArrowUp":
+            dx = 0;
+            dy = -1;
+            break;
+        case "ArrowDown":
+            dx = 0;
+            dy = 1;
+            break;
+        case "ArrowLeft":
+            dx = 1;
+            dy = 0;
+            break;
+        case "ArrowRight":
+            dx = -1;
+            dy = 0;
+            break;
+    }
+}
+
+function update() {
+    for (i = cubes.length - 1; i > 0; i++) {
+        X[i] = X[i - 1];
+        Y[i] = Y[i - 1];
+        cubes[i].style = "left:calc(50% + "+((X[i] - Y[i]) * 121.24355653)+"px); top:calc(50% + "+(X[i] + Y[i]) * 70+"px);";
+    }
+    X[0] += dx;
+    Y[0] += dy;
+    cubes[0].style =  "left:calc(50% + "+((X[0] - Y[0]) * 121.24355653)+"px); top:calc(50% + "+(X[0] + Y[0]) * 70+"px);";
+}
+
+setInterval(update, 100);
